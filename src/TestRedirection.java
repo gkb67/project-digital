3bcbdb134f68be5e1c0506cff96464dfba9b960b
gkb67 <gkutayer@hotmail.com>
2021-12-12 23:18:42 +0100

    - Added parallel browser test - Added tests for Firefox and Safari browsers - Created generated txt for the link details - Updated the location of the browsers' drivers

--- src/TestRedirection.java
+++ src/TestRedirection.java
@@ -1,119 +1,149 @@
 import java.util.List;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
 import java.net.HttpURLConnection;
 import java.net.URL;
-
-
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.safari.SafariDriver;
 import org.testng.Assert;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
-
 
 public class TestRedirection {
 
-	private WebDriver driver;
+	WebDriver driver;
 	private int invalidLinksCount;
+	private int validLinksCount;
+	private String link = "https://www.vanmoof.com/s3";
+	PrintWriter writer;
 
-	
+	/*
+	 * This function executes before each Test tag in redirection.xml
+	 * 
+	 * @param browser
+	 * 
+	 * @throws Exception
+	 */
+	@BeforeTest
+	@Parameters("browser")
+	public void setup(String browserName) throws Exception {
+		if (browserName.equalsIgnoreCase("firefox")) {
+			System.setProperty("webdriver.gecko.driver", "SeleniumDrivers/geckodriver");
+			driver = new FirefoxDriver();
+		} else if (browserName.equalsIgnoreCase("chrome")) {
+			System.setProperty("webdriver.chrome.driver", "SeleniumDrivers/chromedriver");
+			driver = new ChromeDriver();
+		} else if (browserName.equalsIgnoreCase("safari")) {
+			driver = new SafariDriver();
+		}
+	}
+
+	/*
+	 * Test to check if the link is redirected to the exact given link
+	 */
 	@Test
-	public void redirectionWithDefaultColor() {
-		
-		System.setProperty("webdriver.chrome.driver", "/Users/gozde/Documents/Selenium/chromedriver");
-		driver = new ChromeDriver();
-		
-		String link = "https://vanmoof.com/s3";
+	public void redirectionWithDefaultColor() throws InterruptedException {
 		String expectedLink = "https://www.vanmoof.com/en-NL/s3";
-		
+
 		driver.get(link);
-		
+		driver.manage().window().maximize();
+		Thread.sleep(3000);
 		String redirectLink = driver.getCurrentUrl();
-		
-		System.out.println(redirectLink);
-		
 		Assert.assertTrue(redirectLink.contains(expectedLink));
+
 	}
-	
+
+	/*
+	 * Test to check if the link is redirected to the URL which has the given link
+	 */
 	@Test
-	public void redirectionWithoutDefaultColor() {
-		
-		System.setProperty("webdriver.chrome.driver", "/Users/gozde/Documents/Selenium/chromedriver");
-		driver = new ChromeDriver(); //webdriver is interface
-		
-		String link = "https://vanmoof.com/s3";
+	public void redirectionWithoutDefaultColor() throws InterruptedException {
 		String expectedLink = "https://www.vanmoof.com/en-NL/s3";
-		
+
 		driver.get(link);
-		
+		Thread.sleep(3000);
 		String redirectLink = driver.getCurrentUrl();
-		
-		System.out.println(redirectLink);
-		
-		Assert.assertEquals(redirectLink,expectedLink);
+		Assert.assertEquals(redirectLink, expectedLink);
 	}
-	
+
+	/*
+	 * Test to check if each link in the page opens successfully The 'for' loop goes
+	 * through each list element which corresponds to a link and checks its validity
+	 */
 	@Test
-	public void LinksInThePage() {
-		System.setProperty("webdriver.chrome.driver", "/Users/gozde/Documents/Selenium/chromedriver");
-		driver = new ChromeDriver();
-		
-		String link = "https://vanmoof.com/en-NL/s3";
-		
+	public void LinksInThePage() throws FileNotFoundException, Exception {
+
 		driver.get(link);
+		writer = new PrintWriter("VerifyLinksReport.txt", "UTF-8");
+
 		try {
 			invalidLinksCount = 0;
-			List<WebElement> list = driver.findElements(By
-					.tagName("a"));
-			System.out.println("Total number of links are "
-					+ list.size());
+			validLinksCount = 0;
+			List<WebElement> list = driver.findElements(By.tagName("a"));
 			for (WebElement e : list) {
 				if (e != null) {
 					String url = e.getAttribute("href");
-					if (url != null) {
+					if (url != null && !url.contains("javascript")) {
 						verifyLinks(url);
+						validLinksCount++;
 					} else {
 						invalidLinksCount++;
 					}
 				}
 			}
 
-			System.out.println("Total number of invalid links are "
-					+ invalidLinksCount);
+			// if there is an invalid link in the page, the test fails
+			if (invalidLinksCount > 0)
+				Assert.assertTrue(false);
+			writer.close();
 
 		} catch (Exception e) {
 			e.printStackTrace();
 			System.out.println(e.getMessage());
 		}
 	}
-	
-	
-	
-	public void verifyLinks(String linkUrl)
-    {
-        try
-        {
-            URL url = new URL(linkUrl);
 
-            //Create URL connection and get the response code
-            HttpURLConnection httpURLConnect=(HttpURLConnection)url.openConnection();
-            httpURLConnect.setConnectTimeout(5000);
-            httpURLConnect.connect();
-            
-            //Check the status code
-            if(httpURLConnect.getResponseCode()!=200)
-            {
-            	System.out.println(linkUrl + " cannot open successfully - " + httpURLConnect.getResponseCode() + httpURLConnect.getResponseMessage());
-            	invalidLinksCount++;
-            }    
-       
-            else
-            {
-                System.out.println(linkUrl + " opens successfully - " + httpURLConnect.getResponseCode() + httpURLConnect.getResponseMessage());
-            }
-        }
-            catch (Exception e) {
-            	
-      }
-        }
+	/*
+	 * Method to check the response code of the links in the page
+	 */
+	public void verifyLinks(String linkUrl) {
+		try {
+			URL url = new URL(linkUrl);
+
+			// Create URL connection and get the response code
+			HttpURLConnection httpURLConnect = (HttpURLConnection) url.openConnection();
+			httpURLConnect.setConnectTimeout(5000);
+			httpURLConnect.connect();
+
+			// Check the status code
+			if (httpURLConnect.getResponseCode() != 200) {
+				invalidLinksCount++;
+				writer.println(linkUrl + " cannot open successfully - " + httpURLConnect.getResponseCode()
+						+ httpURLConnect.getResponseMessage());
+			}
+
+			else {
+				writer.println(linkUrl + " opens successfully - " + httpURLConnect.getResponseCode()
+						+ httpURLConnect.getResponseMessage());
+			}
+
+		} catch (Exception e) {
+
+		}
+	}
+
+	/*
+	 * This function executes at the end of the tests to close the browsers
+	 * automatically
+	 */
+	@AfterTest
+	public void quitDriver() {
+		driver.close();
+	}
 }
